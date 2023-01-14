7b979dba23f90d74dfc0a4a327e9e12a0ea139cd
Mumtoz Valijonov <mumtoz.v@youpal.es>
2023-01-14 11:47:47 +0100

    Extract menu and validate

--- pyjowi/dto/menu.py
+++ pyjowi/dto/menu.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from pydantic import HttpUrl
+from pydantic import HttpUrl, validator
 
 from ._base import JowiResponseModel
 
@@ -12,15 +12,23 @@ class MenuCategoryCourse(JowiResponseModel):
     price_for_online_order: float
     is_exception: bool
     is_online_order: bool
-    is_vegetarian: bool
-    description: str
+    is_vegetarian: Optional[bool]
+    description: Optional[str]
     image_url: Optional[HttpUrl]
     count_left: Optional[int]
 
     @classmethod
-    def from_jowi_response(cls, response: dict) -> 'MenuCategoryCourse':
-        is_online_order = response.pop('online_order', False)
+    def from_jowi_response(cls, response: dict) -> "MenuCategoryCourse":
+        is_online_order = response.pop("online_order", False)
         return cls(is_online_order=is_online_order, **response)
+
+    @validator("count_left", pre=True)
+    def validate_count_left(cls, v):
+        if v is None:
+            return 0
+        elif isinstance(v, str):
+            return int(float(v))
+        return v
 
 
 class MenuCategory(JowiResponseModel):
@@ -28,17 +36,23 @@ class MenuCategory(JowiResponseModel):
     courses: List[MenuCategoryCourse]
 
     @classmethod
-    def from_jowi_response(cls, response: dict) -> 'MenuCategory':
-        courses_meta = response.get('courses', [])
-        courses = [MenuCategoryCourse.from_jowi_response(course_meta) for course_meta in courses_meta]
-        return cls(title=response['title'], courses=courses)
+    def from_jowi_response(cls, response: dict) -> "MenuCategory":
+        courses_meta = response.get("courses", [])
+        courses = [
+            MenuCategoryCourse.from_jowi_response(course_meta)
+            for course_meta in courses_meta
+        ]
+        return cls(title=response["title"], courses=courses)
 
 
 class Menu(JowiResponseModel):
     categories: List[MenuCategory]
 
     @classmethod
-    def from_jowi_response(cls, response: dict) -> 'Menu':
-        category_metas = response.get('categories', [])
-        categories = [MenuCategory.from_jowi_response(category_meta) for category_meta in category_metas]
+    def from_jowi_response(cls, response: dict) -> "Menu":
+        category_metas = response.get("categories", [])
+        categories = [
+            MenuCategory.from_jowi_response(category_meta)
+            for category_meta in category_metas
+        ]
         return cls(categories=categories)
